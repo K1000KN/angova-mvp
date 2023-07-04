e39049c919b6b105a31ba32d436fc77d19e51472
K1000KN <k1000kn@protonmail.com>
2023-07-04 21:57:24 +0200

    patch the CORS problem when sign-in with vercel.json without headers

--- back/index.js
+++ back/index.js
@@ -13,12 +13,6 @@ import authentificationRoutes from "./src/routes/authentificationRoutes.js";
 const app = express();
 
 const PORT = 3001;
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
