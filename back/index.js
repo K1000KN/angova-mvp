1882333ab8d8ec45a522edef09cb196a6cbee7d0
K1000KN <k1000kn@protonmail.com>
2023-07-04 22:39:00 +0200

    patch the CORS problem when sign-in with index.js

--- back/index.js
+++ back/index.js
@@ -13,16 +13,8 @@ import authentificationRoutes from "./src/routes/authentificationRoutes.js";
 const app = express();
 
 const PORT = 3001;
-const cors = process.env.CORS;
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+//const cors = process.env.CORS;
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
