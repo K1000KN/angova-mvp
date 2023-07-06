8988bbc440cd41b11e226a1b89be822fafaf813e
Mathis Zerbib <mathis.zerbib@epitech.eu>
2023-07-07 01:31:10 +0200

    adapted flags

--- front/src/components/MultiCarousel.js
+++ front/src/components/MultiCarousel.js
@@ -4,15 +4,79 @@ import "react-multi-carousel/lib/styles.css";
 import { makeStyles } from "@mui/styles";
 import { Tooltip } from "@mui/material";
 import { useTranslation } from "react-i18next";
-const franceFlag = "../../images/flag/rounded/france.png";
-const ukFlag = "../../images/flag/rounded/uk.png";
-const algerieFlag = "../../images/flag/rounded/algeria.png";
-const moroccoFlag = "../../images/flag/rounded/morocco.png";
-const tuniFlag = "../../images/flag/rounded/tunisia.png";
-const turkeyFlag = "../../images/flag/rounded/turkey.png";
-const lsfFlag = "../../images/flag/rounded/lsf.png";
-const albaniaFlag = "../../images/flag/rounded/albania.png";
-const ukraineRoundedFlag = "../../images/flag/rounded/ukraine.png";
+
+const flags = [
+  {
+    src: "../../images/flag/rounded/france.png",
+    onClick: () => {
+      // Handle showing France article
+    },
+    isAvailable: true,
+  },
+  {
+    src: "../../images/flag/rounded/spain.png",
+    onClick: () => {
+      // Handle showing Spain article
+    },
+    isAvailable: true,
+  },
+  {
+    src: "../../images/flag/rounded/uk.png",
+    onClick: () => {
+      // Handle showing UK article
+    },
+    isAvailable: true,
+  },
+  {
+    src: "../../images/flag/rounded/algeria.png",
+    onClick: () => {
+      // Handle showing Algeria article
+    },
+    isAvailable: false,
+  },
+  {
+    src: "../../images/flag/rounded/morocco.png",
+    onClick: () => {
+      // Handle showing Morocco article
+    },
+    isAvailable: true,
+  },
+  {
+    src: "../../images/flag/rounded/ukraine.png",
+    onClick: () => {
+      // Handle showing Ukraine article
+    },
+    isAvailable: false,
+  },
+  {
+    src: "../../images/flag/rounded/tunisia.png",
+    onClick: () => {
+      // Handle showing Tunisia article
+    },
+    isAvailable: false,
+  },
+  {
+    src: "../../images/flag/rounded/turkey.png",
+    onClick: () => {
+      // Handle showing Turkey article
+    },
+    isAvailable: false,
+  },
+  {
+    src: "../../images/flag/rounded/lsf.png",
+    onClick: () => {
+      // Handle showing LSF article
+    },
+    isAvailable: true,
+  },
+  {
+    src: "../../images/flag/rounded/albania.png",
+    onClick: () => {
+      // Handle showing Albania article
+    },
+    isAvailable: false,
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   carouselContainer: {
@@ -27,10 +91,6 @@ const useStyles = makeStyles((theme) => ({
     cursor: "pointer",
     width: "auto",
     height: "100%",
-    transition: "filter 0.3s",
-  },
-  tooltip: {
-    zIndex: 999,
   },
 }));
 
@@ -40,27 +100,13 @@ function MultiCarousel() {
   const [tooltipOpen, setTooltipOpen] = useState(null);
   const [tooltipVisible, setTooltipVisible] = useState(false);
 
-  const showMarocArticle = () => {
-    // Handle showing Maroc article
-  };
-
-  const showFrArticle = () => {
-    // Handle showing France article
-  };
-
-  const showUkArticle = () => {
-    // Handle showing UK article
-  };
-
   const handleFlagClick = (flagIndex) => {
-    if (window.innerWidth <= 464) {
-      if (tooltipOpen === flagIndex) {
-        setTooltipOpen(null);
-        setTooltipVisible(false);
-      } else {
-        setTooltipOpen(flagIndex);
-        setTooltipVisible(true);
-      }
+    if (tooltipOpen === flagIndex) {
+      setTooltipOpen(null);
+      setTooltipVisible(false);
+    } else {
+      setTooltipOpen(flagIndex);
+      setTooltipVisible(true);
     }
   };
 
@@ -75,25 +121,25 @@ function MultiCarousel() {
     }
   }, [tooltipOpen]);
 
-  const renderFlag = (src, onClick, className, isAvailable, flagIndex) => {
+  const renderFlag = (flag, flagIndex) => {
     const flagClicked = tooltipOpen === flagIndex;
 
     return (
       <Tooltip
-        title={isAvailable ? t("available") : t("comingSoon")}
+        title={flag.isAvailable ? t("available") : t("comingSoon")}
         placement="top"
-        classes={{ tooltip: classes.tooltip }}
         open={flagClicked && tooltipVisible}
         disableHoverListener
+        key={flagIndex}
       >
         <div
           className={classes.flagImage}
           onClick={() => {
-            onClick();
+            flag.onClick();
             handleFlagClick(flagIndex);
           }}
         >
-          <img src={src} alt="flag" className={className} />
+          <img src={flag.src} alt="flag" className="styleFlag" />
         </div>
       </Tooltip>
     );
@@ -133,15 +179,7 @@ function MultiCarousel() {
       removeArrowOnDeviceType={["tablet", "mobile"]}
       itemClass="carousel-item-padding-40-px"
     >
-      {renderFlag(lsfFlag, showMarocArticle, "styleFlag", true, 1)}
-      {renderFlag(moroccoFlag, showMarocArticle, "styleFlag", true, 2)}
-      {renderFlag(algerieFlag, showMarocArticle, "styleFlag", false, 3)}
-      {renderFlag(tuniFlag, showMarocArticle, "styleFlag", false, 4)}
-      {renderFlag(franceFlag, showFrArticle, "styleFlag", true, 5)}
-      {renderFlag(turkeyFlag, showFrArticle, "styleFlag", false, 6)}
-      {renderFlag(ukFlag, showUkArticle, "styleFlag", true, 7)}
-      {renderFlag(albaniaFlag, showFrArticle, "styleFlag", false, 8)}
-      {renderFlag(ukraineRoundedFlag, showFrArticle, "styleFlag", true, 9)}
+      {flags.map((flag, index) => renderFlag(flag, index))}
     </Carousel>
   );
 }
