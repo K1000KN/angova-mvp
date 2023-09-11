7053ea7a4f89bda95205454b20c614cf4a63a788
Mathis Zerbib <mathis.zerbib@epitech.eu>
2023-09-12 00:27:49 +0200

    clean routes add versioning

--- back/src/routes/s3Routes.js
+++ back/src/routes/s3Routes.js
@@ -4,5 +4,4 @@ import { getAsset } from "../controllers/s3Controller.js";
 const router = express.Router();
 
 router.post("/s3", getAsset);
-router.post("/bucket", getAsset);
 export default router;
