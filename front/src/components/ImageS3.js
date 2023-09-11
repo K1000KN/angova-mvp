d60783a3eb66aa4facb573d750855506b3e1a2ad
Mathis Zerbib <mathis.zerbib@epitech.eu>
2023-09-11 20:40:47 +0200

    change api uri

--- front/src/components/ImageS3.js
+++ front/src/components/ImageS3.js
@@ -4,6 +4,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 import CardMedia from "@mui/material/CardMedia";
 import "./ImageS3.css";
 import Card from "@mui/material/Card";
+const reactApiUrl = process.env.REACT_APP_API_URL;
 const ImageS3 = ({ source }) => {
   const [imageUrl, setImageUrl] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -11,7 +12,7 @@ const ImageS3 = ({ source }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.post("http://localhost:3001/api/v1/s3", {
+        const response = await axios.post(reactApiUrl + "/s3", {
           key: source,
         });
         const imageData = response.data;
